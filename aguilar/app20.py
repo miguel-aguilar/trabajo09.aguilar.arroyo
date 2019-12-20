06a5620bc4810aef85e420fceb56f95c471fba35
miguel agular <maguilarv@unprg.edu.pe>
2019-12-20 12:30:33 -0500

    se agrego el app20

---
+++ aguilar/app20.py
@@ -0,0 +1,13 @@
+import os
+import libreeria
+
+#input
+hora=os.sys.argv[1]
+
+#pedir la funsion
+hor=libreeria.pedir_hora(hora)
+
+#ouput
+print("==================================")
+print("la hora es: " + hor)
+print("==================================")
