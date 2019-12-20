40fa8199c3364b8c49086f795b41840b636342f5
miguel agular <maguilarv@unprg.edu.pe>
2019-12-20 12:34:48 -0500

    se agrego el app25

---
+++ aguilar/app25.py
@@ -0,0 +1,13 @@
+import os
+import libreeria
+
+#input
+telf=os.sys.argv[1]
+
+#pedir la funsion
+tele=libreeria.pedir_telfono(telf)
+
+#ouput
+print("==================================")
+print("mi numreo telefonico es: " + str(tele))
+print("==================================")
