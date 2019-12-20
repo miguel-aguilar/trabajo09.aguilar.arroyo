a9a5b853d7421b86369f9a77dc1c2ddfe7194373
miguel agular <maguilarv@unprg.edu.pe>
2019-12-20 12:34:14 -0500

    se agrego el app24

---
+++ aguilar/app24.py
@@ -0,0 +1,13 @@
+import os
+import libreeria
+
+#input
+correo=os.sys.argv[1]
+
+#pedir la funsion
+cor=libreeria.pedir_correo(correo)
+
+#ouput
+print("==================================")
+print("mi correo es: " + cor)
+print("==================================")
