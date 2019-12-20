800f7f059ddd5e674fcdcc3739c2810f6a45db6d
miguel agular <maguilarv@unprg.edu.pe>
2019-12-20 12:31:39 -0500

    se agrego el app21

---
+++ aguilar/app21.py
@@ -0,0 +1,13 @@
+import os
+import libreeria
+
+#input
+dia=os.sys.argv[1]
+
+#pedir la funsion
+di=libreeria.pedir_dia_semana(dia)
+
+#ouput
+print("==================================")
+print("el dia de hoy es: " + di)
+print("==================================")
